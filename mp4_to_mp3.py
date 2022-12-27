564192baa48abbb9e800ab9ddb129fe539cf47cb
sharik-game <vova2008razor@yandex.ru>
2022-12-27 22:18:17 +0300

    Add logging

--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -1,12 +1,13 @@
 import moviepy.editor
 import logging
+logging.basicConfig(level=logging.INFO, format="%(levelname)s:     %(name)s %(asctime)s %(message)s")
 def mp3(name: str):
     try:
         video = moviepy.editor.VideoFileClip(f"unuseful_cache/{name}.mp4")
         audio = video.audio
         audio.write_audiofile(f"unuseful_cache/{name}.mp3")
-    
+        logging.info("conversion was successful")
     except Exception as ex:
-        print(ex)
+        logging.error("Some error", exc_info=True)
 
 
